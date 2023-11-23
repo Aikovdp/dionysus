fe5f7f60202c3126de3d613af23c6d696365e501
Aiko Vandeputte <aiko@aikovdp.me>
2023-11-23 23:32:47 +0100

    Add general navigation and UI

---
+++ app/src/main/java/me/aikovdp/dionysus/ui/Navigation.kt
@@ -0,0 +1,126 @@
+package me.aikovdp.dionysus.ui
+
+import androidx.annotation.StringRes
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Book
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.BookmarkAdd
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.outlined.Book
+import androidx.compose.material.icons.outlined.BookmarkBorder
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import me.aikovdp.dionysus.R
+import me.aikovdp.dionysus.ui.screens.DiaryScreen
+import me.aikovdp.dionysus.ui.screens.WatchlistScreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview
+fun MainNavigation() {
+    val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val selectedDestination =
+        navBackStackEntry?.destination?.route ?: DionysusScreen.Watchlist.name
+    val screenTitle = DionysusScreen.valueOf(selectedDestination).title
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(title = { Text(stringResource(screenTitle)) })
+        },
+        floatingActionButton = {
+            AnimatedContent(
+                targetState = selectedDestination,
+                transitionSpec = {
+                    scaleIn() togetherWith scaleOut()
+                },
+                label = "FAB Enter / Exit"
+            ) { dest ->
+                when (dest) {
+                    DionysusScreen.Watchlist.name -> {
+                        LargeFloatingActionButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Default.BookmarkAdd,
+                                contentDescription = stringResource(R.string.watchlist_add),
+                                modifier = Modifier.size(28.dp)
+                            )
+                        }
+                    }
+
+                    DionysusScreen.Diary.name -> {
+                        LargeFloatingActionButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Default.Create,
+                                contentDescription = stringResource(R.string.diary_add),
+                                modifier = Modifier.size(28.dp)
+                            )
+                        }
+                    }
+                }
+
+            }
+
+        },
+        bottomBar = {
+            NavigationBar {
+                listOf(DionysusScreen.Watchlist, DionysusScreen.Diary).forEach { item ->
+                    val selected = selectedDestination == item.name
+                    NavigationBarItem(
+                        selected = selected,
+                        onClick = { navController.navigate(item.name) },
+                        icon = {
+                            Icon(
+                                if (selected) item.selectedIcon else item.icon,
+                                stringResource(item.title)
+                            )
+                        },
+                        label = { Text(stringResource(item.title)) }
+                    )
+                }
+            }
+        }
+    ) { paddingValues ->
+        NavHost(navController = navController, startDestination = DionysusScreen.Watchlist.name) {
+
+            composable(DionysusScreen.Watchlist.name) {
+                WatchlistScreen(Modifier.padding(paddingValues))
+            }
+            composable(DionysusScreen.Diary.name) {
+                DiaryScreen(Modifier.padding(paddingValues))
+            }
+        }
+
+    }
+}
+
+enum class DionysusScreen(
+    @StringRes val title: Int,
+    val icon: ImageVector,
+    val selectedIcon: ImageVector = icon
+) {
+    Watchlist(R.string.watchlist, Icons.Outlined.BookmarkBorder, Icons.Default.Bookmark),
+    Diary(R.string.diary, Icons.Outlined.Book, Icons.Default.Book)
+}
---
+++ app/src/main/java/me/aikovdp/dionysus/ui/screens/DiaryScreen.kt
@@ -0,0 +1,12 @@
+package me.aikovdp.dionysus.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DiaryScreen(modifier: Modifier = Modifier) {
+    Text("Diary", modifier)
+}
+
+
