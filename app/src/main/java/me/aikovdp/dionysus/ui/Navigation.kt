de4d05392babd5377cfe660e502f34700ceb4f59
Aiko Vandeputte <aiko@aikovdp.me>
2023-11-24 11:54:24 +0100

    Correct icon sizes in Large FABs

--- app/src/main/java/me/aikovdp/dionysus/ui/Navigation.kt
+++ app/src/main/java/me/aikovdp/dionysus/ui/Navigation.kt
@@ -16,6 +16,7 @@ import androidx.compose.material.icons.outlined.BookmarkBorder
 import androidx.compose.material.icons.outlined.Create
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.NavigationBar
@@ -28,7 +29,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -65,7 +65,7 @@ fun MainNavigation() {
                             Icon(
                                 imageVector = Icons.Outlined.BookmarkAdd,
                                 contentDescription = stringResource(R.string.add_watchlist_entry),
-                                modifier = Modifier.size(28.dp)
+                                modifier = Modifier.size(FloatingActionButtonDefaults.LargeIconSize)
                             )
                         }
                     }
@@ -75,7 +75,7 @@ fun MainNavigation() {
                             Icon(
                                 imageVector = Icons.Outlined.Create,
                                 contentDescription = stringResource(R.string.add_diary_entry),
-                                modifier = Modifier.size(28.dp)
+                                modifier = Modifier.size(FloatingActionButtonDefaults.LargeIconSize)
                             )
                         }
                     }
