6413e22a9e56e2ef614f56ef45ea827c9c28f5ca
Aiko Vandeputte <aiko@aikovdp.me>
2023-12-27 16:33:34 +0100

    Show watchlist entries on screen

--- app/src/main/java/me/aikovdp/dionysus/ui/screens/watchlist/WatchlistScreen.kt
+++ app/src/main/java/me/aikovdp/dionysus/ui/screens/watchlist/WatchlistScreen.kt
@@ -1,6 +1,8 @@
 package me.aikovdp.dionysus.ui.screens.watchlist
 
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.BookmarkAdd
 import androidx.compose.material3.FloatingActionButtonDefaults
@@ -8,13 +10,24 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
 import me.aikovdp.dionysus.R
 
 @Composable
-fun WatchlistScreen(modifier: Modifier = Modifier) {
-    Text("Watchlist", modifier)
+fun WatchlistScreen(
+    modifier: Modifier = Modifier,
+    viewModel: WatchlistViewModel = hiltViewModel(),
+) {
+    val uiState by viewModel.uiState.collectAsState()
+    LazyColumn(modifier = modifier) {
+        items(uiState.items) {
+            Text(text = it.movie.title)
+        }
+    }
 }
 
 @Composable
