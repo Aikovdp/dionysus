73b443fcbb0b3c5c750e0c01b06d551be90e3c92
Aiko Vandeputte <aiko@aikovdp.me>
2023-12-28 00:00:44 +0100

    Show images on watchlist screen

--- app/src/main/java/me/aikovdp/dionysus/ui/screens/watchlist/WatchlistScreen.kt
+++ app/src/main/java/me/aikovdp/dionysus/ui/screens/watchlist/WatchlistScreen.kt
@@ -1,10 +1,16 @@
 package me.aikovdp.dionysus.ui.screens.watchlist
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.BookmarkAdd
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LargeFloatingActionButton
@@ -13,9 +19,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 import me.aikovdp.dionysus.R
+import me.aikovdp.dionysus.data.Movie
+import me.aikovdp.dionysus.data.WatchlistEntry
+import me.aikovdp.dionysus.ui.theme.Typography
+import java.time.Instant
 
 @Composable
 fun WatchlistScreen(
@@ -23,11 +37,7 @@ fun WatchlistScreen(
     viewModel: WatchlistViewModel = hiltViewModel(),
 ) {
     val uiState by viewModel.uiState.collectAsState()
-    LazyColumn(modifier = modifier) {
-        items(uiState.items) {
-            Text(text = it.movie.title)
-        }
-    }
+    WatchlistGrid(items = uiState.items, modifier = modifier)
 }
 
 @Composable
@@ -39,4 +49,63 @@ fun WatchlistFab(onClick: () -> Unit) {
             modifier = Modifier.size(FloatingActionButtonDefaults.LargeIconSize)
         )
     }
+}
+
+@Composable
+fun WatchlistGrid(items: List<WatchlistEntry>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier,
+        contentPadding = PaddingValues(start = 16.dp, end = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(items) {
+            WatchlistGridEntry(it)
+        }
+    }
+}
+
+@Composable
+fun WatchlistGridEntry(entry: WatchlistEntry) {
+    ElevatedCard {
+        AsyncImage(
+            model = entry.movie.posterUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = entry.movie.title,
+            style = Typography.titleMedium,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WatchlistGridPreview() {
+    val list = listOf(
+        WatchlistEntry(
+            1,
+            Movie(
+                1,
+                "La La Land",
+                "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/uDO8zWDhfWwoFdKS4fzkUJt0Rf0.jpg"
+            ),
+            Instant.now()
+        ),
+        WatchlistEntry(
+            2,
+            Movie(
+                2,
+                "Parasite",
+                "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg"
+            ),
+            Instant.now()
+        )
+    )
+
+    WatchlistGrid(list)
 }
