9706f4ea012ab1c26523aa62a47235ee538a39eb
Aiko Vandeputte <aiko@aikovdp.me>
2023-12-27 15:32:07 +0100

    Add Hilt annotations

---
+++ app/src/main/java/me/aikovdp/dionysus/DionysusApplication.kt
@@ -0,0 +1,7 @@
+package me.aikovdp.dionysus
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DionysusApplication: Application()
--- app/src/main/java/me/aikovdp/dionysus/MainActivity.kt
+++ app/src/main/java/me/aikovdp/dionysus/MainActivity.kt
@@ -3,9 +3,11 @@ package me.aikovdp.dionysus
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 import me.aikovdp.dionysus.ui.MainNavigation
 import me.aikovdp.dionysus.ui.theme.DionysusTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
