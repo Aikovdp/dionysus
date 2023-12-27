32dc8f0945e07d4228537c94328721bb8ff24579
Aiko Vandeputte <aiko@aikovdp.me>
2023-12-28 00:02:47 +0100

    Update hilt dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,8 +74,8 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-android-compiler:2.50")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     // Coil
