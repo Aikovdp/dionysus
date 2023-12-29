148b23abc969373c7d824d4f1e4435f3fc8fa796
Aiko Vandeputte <aiko@aikovdp.me>
2023-12-29 21:43:13 +0100

    Add Dokka And disable configuration cache

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,15 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
+    id("org.jetbrains.dokka") version "1.9.10"
 }
 
 buildscript {
     dependencies {
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
     }
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
 }
