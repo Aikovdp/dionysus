204f564c4cbe80b60a3c2609befcb1f08ce5293e
Aiko Vandeputte <aiko@aikovdp.me>
2023-12-30 22:11:26 +0100

    Use automirrored back arrow

--- app/src/main/java/me/aikovdp/dionysus/ui/screens/movie/MovieScreen.kt
+++ app/src/main/java/me/aikovdp/dionysus/ui/screens/movie/MovieScreen.kt
@@ -9,7 +9,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -50,7 +50,7 @@ fun MovieDetailScreen(
                 scrollBehavior = scrollBehavior,
                 navigationIcon = {
                     IconButton(onClick = navigateUp) {
-                        Icon(Icons.Default.ArrowBack, "Back")
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, "Back")
                     }
                 },
             )
