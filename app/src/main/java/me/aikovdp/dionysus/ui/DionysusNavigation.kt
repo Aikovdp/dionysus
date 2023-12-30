a4113c14d83e33ac2f82a405bf07479a1d2c0d38
Aiko Vandeputte <aiko@aikovdp.me>
2023-12-30 14:30:45 +0100

    Don't restore top level destination state

--- app/src/main/java/me/aikovdp/dionysus/ui/DionysusNavigation.kt
+++ app/src/main/java/me/aikovdp/dionysus/ui/DionysusNavigation.kt
@@ -54,14 +54,10 @@ class DionysusNavigationActions(private val navController: NavHostController) {
             // Pop up to the start destination of the graph to
             // avoid building up a large stack of destinations
             // on the back stack as users select items
-            popUpTo(navController.graph.findStartDestination().id) {
-                saveState = true
-            }
+            popUpTo(navController.graph.findStartDestination().id)
             // Avoid multiple copies of the same destination when
             // reselecting the same item
             launchSingleTop = true
-            // Restore state when reselecting a previously selected item
-            restoreState = true
         }
     }
 }
