e24b20fe2f176988652c9623322971cd6e1fca60
Aiko Vandeputte <aiko@aikovdp.me>
2024-01-06 15:54:05 +0100

    Refactor movie and poster base url

--- app/src/main/java/me/aikovdp/dionysus/data/ModelMappingExt.kt
+++ app/src/main/java/me/aikovdp/dionysus/data/ModelMappingExt.kt
@@ -6,6 +6,9 @@ import me.aikovdp.dionysus.data.source.network.NetworkMovieDetails
 import java.time.LocalDate
 import kotlin.time.Duration.Companion.minutes
 
+const val posterBaseUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/"
+const val backdropBaseUrl = "https://www.themoviedb.org/t/p/w1280/"
+
 fun NetworkMovie.toExternal() = Movie(
     id = id,
     title = title,
@@ -15,8 +18,8 @@ fun NetworkMovie.toExternal() = Movie(
 fun NetworkMovieDetails.toExternal() = MovieDetails(
     id = id,
     title = title,
-    posterUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/$posterPath",
-    backdropUrl = "https://www.themoviedb.org/t/p/w1280/$backdropPath",
+    posterUrl = "$posterBaseUrl$posterPath",
+    backdropUrl = "$backdropBaseUrl$backdropPath",
     releaseDate = LocalDate.parse(releaseDate),
     runtime = runtime.minutes,
     tagline = tagline,
